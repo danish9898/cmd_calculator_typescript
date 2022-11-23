2cf3f7a7954377378eb63e7447926f173c941ae5
yayym001 <muhammad.tayyed@vcontractor.co.za>
2022-11-23 16:08:45 +0500

    latest update

--- index.ts
+++ index.ts
@@ -1,27 +1,11 @@
-import { question } from "readline-sync";
+import { question } from "readline-sync"; //  use this for the input ,It provide buildin method
 
 // type Operator = "+" | "-" | "*" | "/"; // we also use for pre define operation for more reeadable and use as calculator function parameter (operator : Operator) and calculator function calculate(firstNum, operator as Operator, secondNum)
 
-function main(): void {
-  const firstStr: string = question("Enter the first number:\n");
-  const operator: string = question("Enter operation:\n");
-  const secondStr: string = question("Enter secondnumber:\n");
-
-  const validInput: boolean =
-    isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
-  if (validInput) {
-    console.log("valid");
-    const firstNum: number = parseInt(firstStr);
-    const secondNum: number = parseInt(secondStr);
-    const result = calculate(firstNum, operator, secondNum);
-    console.log(result);
-  } else {
-    console.log("\ninvalid input\n");
-    main();
-  }
-}
 function calculate(firstNum: number, operator: string, secondNum: number) {
-  switch (operator) {
+  switch (
+    operator // Alternative of switch is if else condition
+  ) {
     case "+":
       return firstNum + secondNum;
     case "-":
@@ -49,4 +33,23 @@ function isNumber(str: string): boolean {
   const isNum: boolean = !isNaN(maybeNum);
   return isNum;
 }
+
+function main(): void {
+  const firstStr: string = question("Enter the first number:\n");
+  const operator: string = question("Enter operation:\n");
+  const secondStr: string = question("Enter secondnumber:\n");
+
+  const validInput: boolean =
+    isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+  if (validInput) {
+    const firstNum: number = parseInt(firstStr);
+    const secondNum: number = parseInt(secondStr);
+    const result = calculate(firstNum, operator, secondNum);
+    console.log("Result = " + result);
+  } else {
+    console.log("\ninvalid input\n");
+    main();
+  }
+}
+
 main();
